603b5942da8e4ce9fdf4070567108385562f1924
xia <xiahui5@163.com>
2021-05-18 11:24:00 +0800

    test

--- myDemo/src/test/java/OneTest.java
+++ myDemo/src/test/java/OneTest.java
@@ -1,7 +1,7 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TestOne {
+public class OneTest {
     @Test
     public void test_one(){
         Assert.assertTrue(true);
